9f8e3d29c9ff305016ff6da2b30fd6eeecf1d8b9
enzoftware <lizama.enzo@gmail.com>
2019-12-09 22:35:08 -0500

    add config and setup app

---
+++ app/src/main/java/com/enzoftware/mvvmarchexample/GithubApp.kt
@@ -0,0 +1,4 @@
+package com.enzoftware.mvvmarchexample
+
+class GithubApp {
+}
---
+++ app/src/main/java/com/enzoftware/mvvmarchexample/util/Config.kt
@@ -0,0 +1,5 @@
+package com.enzoftware.mvvmarchexample.util
+
+object Config {
+    const val API_ENDPOINT = "https://api.github.com/"
+}
