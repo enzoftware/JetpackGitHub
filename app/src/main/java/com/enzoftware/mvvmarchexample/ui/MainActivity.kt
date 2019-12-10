29ef9050c9eceaba91766819f69d55276ec17bf3
enzoftware <lizama.enzo@gmail.com>
2019-12-09 22:34:51 -0500

    initial ui commit

--- app/src/main/java/com/enzoftware/mvvmarchexample/ui/MainActivity.kt
+++ app/src/main/java/com/enzoftware/mvvmarchexample/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.enzoftware.mvvmarchexample
+package com.enzoftware.mvvmarchexample.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.enzoftware.mvvmarchexample.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
--- app/src/main/java/com/enzoftware/mvvmarchexample/util/Keyboard.kt
+++ app/src/main/java/com/enzoftware/mvvmarchexample/util/Keyboard.kt
@@ -1,5 +1,19 @@
 package com.enzoftware.mvvmarchexample.util
 
+import android.content.Context
+import android.os.IBinder
+import android.view.inputmethod.InputMethodManager
+
 object Keyboard {
+
+    fun show(context: Context) {
+        val manager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+
+    fun hide(windowToken: IBinder, context: Context){
+        val manager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.hideSoftInputFromWindow(windowToken, 0)
+    }
 
 }
