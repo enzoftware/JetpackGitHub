59c96217bff36629a7ed3e3e02c8db8c34d649a6
Enzo Lizama <lizama.enzo@gmail.com>
2020-01-08 23:47:22 -0500

    add github app setup dagger application

--- app/src/main/java/com/enzoftware/mvvmarchexample/GithubApp.kt
+++ app/src/main/java/com/enzoftware/mvvmarchexample/GithubApp.kt
@@ -1,4 +1,32 @@
 package com.enzoftware.mvvmarchexample
 
-class GithubApp {
+import android.content.Context
+import com.enzoftware.mvvmarchexample.di.modules.NetworkModule
+import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+
+/**
+ *
+ *Application class. [DaggerApplication] inject classes behind the scene.
+ */
+open class GithubApp : DaggerApplication() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        initAndroidThreeTen()
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder()
+            .application(this)
+            .networkModule(NetworkModule())
+            .build()
+    }
+
+    protected open fun initAndroidThreeTen() {
+        AndroidThreeTen.init(this)
+    }
 }
+
