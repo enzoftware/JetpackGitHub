ebcc76ade584609f9953515b85b8e5c059f93282
Enzo Lizama <lizama.enzo@gmail.com>
2020-01-08 23:46:56 -0500

    add config api endpoint

---
+++ app/src/main/java/com/enzoftware/mvvmarchexample/Config.kt
@@ -0,0 +1,11 @@
+package com.enzoftware.mvvmarchexample
+
+
+/**
+ * Created by Enzo Lizama Paredes on 2020-01-08.
+ * Contact: lizama.enzo@gmail.com
+ */
+
+object Config {
+    const val API_ENDPOINT = "https://api.github.com/"
+}
