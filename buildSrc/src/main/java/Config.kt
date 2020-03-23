66a34929951ef719e4e3757cfbda33ab4e07ff82
Enzo Lizama <lizama.enzo@gmail.com>
2020-03-22 22:53:10 -0500

    fix config package name

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,7 +1,7 @@
 object Config {
     const val versionName = "1.0.0"
     const val versionCode = 1
-    const val packageName = "com.enzoftware.jetpackgithub"
+    const val packageName = "com.enzoftware.mvvmarchexample"
     const val compileSdkVersion = 28
     const val targetSdkVersion = 28
     const val minSdkVersion = 26
