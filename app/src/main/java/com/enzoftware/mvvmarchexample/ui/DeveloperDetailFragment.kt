577658c1cb3a05afbb1d9749778b9f36da99d3d9
Enzo Lizama <lizama.enzo@gmail.com>
2020-03-22 22:53:56 -0500

    add fragments for ui implementation

---
+++ app/src/main/java/com/enzoftware/mvvmarchexample/ui/DeveloperDetailFragment.kt
@@ -0,0 +1,24 @@
+package com.enzoftware.mvvmarchexample.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.enzoftware.mvvmarchexample.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DeveloperDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_developer_detail, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/enzoftware/mvvmarchexample/ui/MainTabFragment.kt
@@ -0,0 +1,24 @@
+package com.enzoftware.mvvmarchexample.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.enzoftware.mvvmarchexample.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainTabFragment : Fragment(R.layout.fragment_main_tab) {
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_main_tab, container, false)
+//    }
+
+}
